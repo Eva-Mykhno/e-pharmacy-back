3fb1bb177a3aebd9a4131f7daab1ea39f4f7bf3e
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2025-03-10 10:31:36 +0200

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -30,6 +30,7 @@ export const loginUserController = async (req, res) => {
     status: 200,
     message: "Successfully logged in!",
     data: {
+user,
       accessToken: session.accessToken,
       refreshToken: session.refreshToken,
     },
