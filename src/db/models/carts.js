038ed18555155fe596c11d782fb867f501f8e970
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2025-03-13 16:47:15 +0200

    Update carts.js

--- src/db/models/carts.js
+++ src/db/models/carts.js
@@ -18,10 +18,6 @@ const cartSchema = new Schema(
           type: String,
           required: true,
         },
-        category: {
-          type: String,
-          required: true,
-        },
         price: {
           type: Number,
           required: true,
